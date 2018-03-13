b7620fe56429ba4f58fe12bbc146cfefb68b4f44
buddy1987 <nguyentrungnam47v@gmail.com>
2018-03-13 07:46:59 +0700

    Bố cục 2 phần: 1. Khai thác kq HQ tại các điểm xuất 1 lần chạy; lưu output thư mục out_hd; vẽ pic ở thư mục figs; 2.Khai thác BOD; DO như HQ và lưu ở out_bod

--- R/Sal_2_Excel.R
+++ R/Sal_2_Excel.R
@@ -16,54 +16,178 @@ library(here)
 library(stringr)
 library(readxl)
 library(dplyr)
+library(ggplot2)
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ipak function: install and load multiple R packages.
+# check to see if packages are installed. Install them if they are not, then load them into the R session.
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# usage
+packages <- c("sp", "here", "stringr", "XLConnect", "sp", "maptools","purrr","plyr","readxl","dplyr")
+ipak(packages)
+
+## Khai bao thu mục và số lần chạy nếu cần
+pa = "PA0_BOD_0"  # Thư mục lưu kq
+lan = ""       # số lần chạy
 
 # Load danh sach tram ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-df = read_excel(paste0("d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/input_new","/ds_Export_Mc_RU.xlsx"),
-                sheet = 1)
+df = read_excel(paste0(here::here(),"/rawdata/","ds_Export_Mc_RU.xlsx"),sheet = 1)
 
 # Xuất mực nước giờ tại Hydro station ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-df_HH = read.table(paste0("d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/test","/HD_SAL.HHH"),
-                   skip = 3,header = F)
+df_HH = read.table(paste0(here::here(),"/rawdata/sal_S/",pa,"/HD_SAL.HHH"),skip = 3,header = F)
 ## Lay ten tram do
 n = na.omit(as.character(df$mc))
-name = c("tt","h","d","t","y",paste0("mc",n))
+name = c("tt","h","d","m","y",paste0("mc",n))
 colnames(df_HH) = name
-write.table(df_HH, file = "d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/output/df_HHH.csv", sep = ",", row.names = F)
+write.table(df_HH, file = paste0(here::here(),"/delta_sim/out_hd","/df_H_",pa,"_",lan,".csv"), sep = ",", row.names = F)
+df_HH = gather(df_HH,mc,value,-h,-d,-m,-y,-tt)
+## vẽ biểu đồ tự động
+df_HH$date = as.Date(paste(df_HH$d,df_HH$m,df_HH$y,sep ="-"),format = "%d-%m-%Y")
+lplotr <- function(x, na.rm = TRUE, ...){
+  sta_list = unique(x$mc)
+  for (i in seq_along(sta_list)) {
+    p <-ggplot(subset(x,x$mc == sta_list[i]),aes(date,value,colour = mc))
+    plots = p + geom_line()+
+      ggtitle(paste("Dien bien H tại MC",sta_list[i],pa))+
+      scale_y_continuous("unit(m)")
+    setwd(paste0(here::here(),"/figs/"))
+    ggsave(plots,filename= paste("H-",sta_list[i],"_",pa,lan,".png",sep=""),width = 22, height = 11,scale = 0.5)
+  }
+}
+lplotr(df_HH)
 
+# Xuất măn giờ tại Hydro station ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+df_HH = read.table(paste0(here::here(),"/rawdata/sal_S/",pa,"/HD_SAL.SSS"),skip = 3,header = F)
+## Lay ten tram do
+n = na.omit(as.character(df$man))
+name = c("tt","h","d","m","y",paste0("mc",n))
+colnames(df_HH) = name
+write.table(df_HH, file = paste0(path_out,"/df_SSS_",lan,".csv"), sep = ",", row.names = F)
+df_HH = gather(df_HH,mc,value,-h,-d,-m,-y,-tt)
+## vẽ biểu đồ tự động
+df_HH$date = as.Date(paste(df_HH$d,df_HH$m,df_HH$y,sep ="-"),format = "%d-%m-%Y")
+lplotr <- function(x, na.rm = TRUE, ...){
+  sta_list = unique(x$mc)
+  for (i in seq_along(sta_list)) {
+    p <-ggplot(subset(x,x$mc == sta_list[i]),aes(date,value,colour = mc))
+    plots = p + geom_line()+
+      ggtitle(paste("Dien bien mặn tại MC",sta_list[i],lan))+
+      scale_y_continuous("unit(g/l)")
+    setwd(paste0(here::here(),"/figs/"))
+    ggsave(plots,filename= paste("S-",sta_list[i],"_",pa,lan,".png",sep=""),width = 22, height = 11,scale = 0.5)
+  }
+}
+lplotr(df_HH)
 
 # Xuất LƯU LƯƠNG giờ tại Hydro station ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-df_HH = read.table(paste0("d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/test","/HD_SAL.QQQ"),
-                   skip = 3,header = F)
+df_HH = read.table(paste0(here::here(),"/rawdata/sal_S/",pa,"/HD_SAL.QQQ"),skip = 3,header = F)
 ## Lay ten tram do
 n = na.omit(as.character(df$mc))
-name = c("tt","h","d","t","y",paste0("mc",n))
+name = c("tt","h","d","m","y",paste0("mc",n))
 colnames(df_HH) = name
-#df_HH = df_HH[,1:6]
-write.table(df_HH, file = "d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/output/df_QQQ.csv", sep = ",", row.names = F)
+write.table(df_HH, file = paste0(here::here(),"/delta_sim/out_hd","/df_Q_",pa,"_",lan,".csv"), sep = ",", row.names = F)
+df_HH = gather(df_HH,mc,value,-h,-d,-m,-y,-tt)
+## vẽ biểu đồ tự động
+df_HH$date = as.Date(paste(df_HH$d,df_HH$m,df_HH$y,sep ="-"),format = "%d-%m-%Y")
+lplotr <- function(x, na.rm = TRUE, ...){
+  sta_list = unique(x$mc)
+  for (i in seq_along(sta_list)) {
+    p <-ggplot(subset(x,x$mc == sta_list[i]),aes(date,value,colour = mc))
+    plots = p + geom_line()+
+      ggtitle(paste("Dien bien Flow tại MC",sta_list[i],lan))+
+      scale_y_continuous("unit(m3/s)")
+    setwd(paste0(here::here(),"/figs/"))
+    ggsave(plots,filename= paste("Q-",sta_list[i],"_",pa,lan,".png",sep=""),width = 22, height = 11,scale = 0.5)
+  }
+}
+lplotr(df_HH)
 
 
-# Xuất MỰC NƯỚC TGIỜ TAi Ruộng ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-df_HH = read.table(paste0("d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/test","/HD_SAL.HPL"),
-                   skip = 2,header = F)
-## Lay ten tram do
-n = na.omit(as.character(df$ru))
-name = c("tt","h","d","t","y",paste0("HPL",n))
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Section: Xuat ket quả cua BOD & DO, ToN; ToP
+# Purpose:
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+pa = "PA0_BOD"
+lan = ""
+# Khai thác DO
+# Load danh sach tram ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+df = read_excel(paste0(here::here(),"/rawdata/","ds_Export_Mc_RU.xlsx"),sheet = 1)
+# Xuất DO tai MC ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+n = na.omit(as.character(df$do))
+name = c("tt","h","d","m","y",n)
+df_HH = read.table(paste0(here::here(),"/rawdata/sal_S/",pa,"/HD_SAL.OXY"),skip = 3,header = F)
 colnames(df_HH) = name
-write.table(df_HH, file = "d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/output/df_HPL.csv", sep = ",", row.names = F)
+## ghi ra bang full cua DO
+write.table(df_HH, file = paste0(path_outwq,"/df_DO_full_",pa,".csv"), sep = ",", row.names = F)
+df_HH$tt = NULL
+df_HH = gather(df_HH,mc,value,-h,-d,-m,-y)
+## vẽ biểu đồ tự động
+df_HH$date = as.Date(paste(df_HH$d,df_HH$m,df_HH$y,sep ="-"),format = "%d-%m-%Y")
+lplotr <- function(x, na.rm = TRUE, ...){
+  sta_list = unique(x$mc)
+  for (i in seq_along(sta_list)) {
+    p <-ggplot(subset(x,x$mc == sta_list[i]),aes(date,value,colour = mc))
+    plots = p + geom_line()+
+      ggtitle(paste("Dien bien DO tại MC",sta_list[i],pa))+
+      scale_y_continuous("unit(mg/l)")
+    setwd(paste0(here::here(),"/figs/"))
+    ggsave(plots,filename= paste("DO-MC",sta_list[i],"_",pa,".png",sep=""),width = 22, height = 11,scale = 0.5)
+  }
+}
+lplotr(df_HH)
+
+## lấy bảng lọc 
+df = read_excel(paste0(here::here(),"/rawdata/","ds_Export_Mc_RU.xlsx"),sheet = 2)
+df[] <- lapply(df, as.character)
+df_HH[] <- lapply(df_HH, as.character)
+df = left_join(df,df_HH,by = c("d","m","y","mc"))
+df[] <- lapply(df, as.numeric)
+df$id = paste(df$mc,df$m,df$d,df$h,sep = "_")
+df = subset(df,select = c("id","mc","h","d","m","y","value"))
+write.table(df, file = paste0(path_outwq,"/df_DO.csv"), sep = ",", row.names = F)
 
 
-# Xuất Dòng chảy giờ tại Ruộng ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-df_HH = read.table(paste0("d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/test","/HD_SAL.QPL"),
-                   skip = 2,header = F)
-## Lay ten tram do
-n = na.omit(as.character(df$ru))
-name = c("tt","h","d","t","y",paste0("QPL",n))
+# Khai thác BOD
+# Load danh sach tram ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+df = read_excel(paste0(here::here(),"/rawdata/","ds_Export_Mc_RU.xlsx"),sheet = 1)
+# Xuất DO tai MC ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+n = na.omit(as.character(df$do))
+name = c("tt","h","d","m","y",n)
+df_HH = read.table(paste0(here::here(),"/rawdata/sal_S/",pa,"/HD_SAL.BOD"),skip = 3,header = F)
 colnames(df_HH) = name
-write.table(df_HH, file = "d:/Trung Nam/SIWRP/A Dung/Cailon_caibe/Simulation_delta/output/df_QPL.csv", sep = ",", row.names = F)
+## ghi ra bang full cua BOD
+write.table(df_HH, file = paste0(path_outwq,"/df_BOD_full_",pa,".csv"), sep = ",", row.names = F)
+df_HH$tt = NULL
+df_HH = gather(df_HH,mc,value,-h,-d,-m,-y)
 
+## vẽ biểu đồ tự động
+df_HH$date = as.Date(paste(df_HH$d,df_HH$m,df_HH$y,sep ="-"),format = "%d-%m-%Y")
+lplotr <- function(x, na.rm = TRUE, ...){
+  sta_list = unique(x$mc)
+  for (i in seq_along(sta_list)) {
+    p <-ggplot(subset(x,x$mc == sta_list[i]),aes(date,value,colour = mc))
+    plots = p + geom_line()+
+      ggtitle(paste("Dien bien BOD tại MC",sta_list[i],pa))+
+      scale_y_continuous("unit(mg/l)")
+    setwd(paste0(here::here(),"/figs/"))
+    ggsave(plots,filename= paste("BOD-MC",sta_list[i],"_",pa,".png",sep=""),width = 22, height = 11,scale = 0.5)
+  }
+}
+lplotr(df_HH)
 
-
-
-
-
+## lấy bảng lọc 
+df = read_excel(paste0(here::here(),"/rawdata/","ds_Export_Mc_RU.xlsx"),sheet = 2)
+df[] <- lapply(df, as.character)
+df_HH[] <- lapply(df_HH, as.character)
+df = left_join(df,df_HH,by = c("d","m","y","mc"))
+df[] <- lapply(df, as.numeric)
+df$id = paste(df$mc,df$m,df$d,df$h,sep = "_")
+df = subset(df,select = c("id","mc","h","d","m","y","value"))
+write.table(df, file = paste0(path_outwq,"/df_BOD.csv"), sep = ",", row.names = F)
